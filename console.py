00386b068cad5373282035d9b75cc287b5809fb0
AchrafHalal <achraf.halal.ax@gmail.com>
2023-12-05 21:05:32 +0100

    EOF signal to exit the prgrm

--- console.py
+++ console.py
@@ -8,7 +8,12 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
         pass
-    
+
     def do_quit(self, arg):
         """Quit command to exit the program."""
+        return True
+    
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
         return True
