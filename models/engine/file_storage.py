afed0cc59311c305cbabb1d48baf48a42d51ca22
AchrafHalal <achraf.halal.ax@gmail.com>
2023-12-05 20:39:32 +0100

    create a function that set a obj with a key

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,5 +14,10 @@ class FileStorage:
     def all(self):
         """Return the dictionary __objects."""
         return FileStorage.__objects
+    
+    def new(self, obj):
+        """Set in __objects obj with key <obj_class_name>.id"""
+        obj_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(obj_name, obj.id)] = obj
 
    
